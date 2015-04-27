0a86b1e46d408eb683ca60c26f130b931c387b42
bogomez <bogomez@tracasa.es>
2015-04-27 17:48:09 +0200

    Avanzo en el libro me quedo en el capitulo-3

--- javascript types.md
+++ javascript types.md
@@ -8,6 +8,7 @@ Los tipos vienen definidos por el valor y no por la variable</p>
 + string
 + boolean
 + Symbol
+
 #Operador typeof
 + typeof {} === 'object'
 + typeof null === 'object'
@@ -20,6 +21,12 @@ Los tipos vienen definidos por el valor y no por la variable</p>
   + typeof [] = "object"
   + typeof function(){} = "function"
 
+#value vs ref
++ En javascript no se comparten las referencias
++ El tipo del valor de la variable define si se pasa como valor o como referencia
+  + Por valor: null, undefined, string, number, boolean, symbol
+  + Por referencia: objects
+
 #Arrays
 + Contenedor de otros tipos
 + Es un objeto que contiene propiedades numericas indexables
@@ -28,7 +35,47 @@ Los tipos vienen definidos por el valor y no por la variable</p>
 + Es muy facil generar sparse arrays. ej. var a = []; a[1] = 1;
 + Detectar un array mediante Array.isArray(obj) (ES5);
 
-# Arrays like
+#Arrays like
 + Usar slice.
 + Usar fx del prototipo + call
 + Array.from (ES6)
+
+#String
++ Inmutables
+
+#Numbers
++ reprented as: 
+  + base-10. 42, 42.12, 0.23 
+  + Exponencial. 1E6
+  + Hexadecimal. 0xf3
+  + Octal. 0363 Not allowed in ES6 strict mode you must instead use 0o363
++ Comparar coma flotante 
++ Number epsilon (ES6)
+
+#Special values
++ undefined
+  + Valor no definido
+  + En non-strict mode se puede modificar la variable global undefined (NO LO HAGAS!!)
+  + En strict y non-strict mode se pueden crear variables locales con el nombre undefined (NO LO HAGAS!!!)
++ null --> valor vacio
++ void es un operador global que siempre devuelve el valor real de undefined
++ NaN
+  + Cualquier operación matemática en la que uno de los operandos no sea un número devolverá NaN
+  + Es el único valor que es distinto a si mismo. NaN === NaN // false
+  + Su tipo es number
+  + isNaN indica si es un NaN pero retorna true para ciertos valores no numéricos: undefined, string, function.
+  + Number.isNaN funciona bien
++ Infinity
+  + Se generan cuando una operación matemática da error siendo sus dos operandos numéricos.
+  + Dos tipos: Infinity, -Infinity
+  + Se puede comprovar si un valor es Infinity de tres formas
+    + Comparando el valor con Number.POSITIVE_INFINITY
+    + Comparando el valor con Number.NEGATIVE_INFINITY
+    + Pasando el valor a la función Number.isFinite()
++ Zeros
+  + Dos tipos +0, -0
+  + cero negativo
+    + Si lo pasamos a string pasa como 0
+    + Si lo pasamos de string a número pasa como -0
+    + Si lo comparamos con +0 indica que son iguales
+* Object.is compara de forma stricta la igualdad de valores funciona correctamente incluso para NaN y -0
--- tests/types.spec.js
+++ tests/types.spec.js
@@ -1,13 +1,132 @@
 (function(){
+  "use strict"
   var expect = chai.expect;
   describe("javascript: tipos", function(){
+    describe("Value vs reference", function(){
+      it("Los valores asignados por valor son inmutables", function(){
+        var a,b;
+        a = 25;
+        b = a;
+        expect(a).to.be.equals(b);
+        b = "texto";
+        expect(a).not.to.be.equals(b);
+        expect(a).to.be.equals(25);
+        expect(b).to.be.equals("texto");
+      });
+      it("Los valores por referencia se pueden modificar pero intercambiar", function(){
+        var a,b;
+        a = {propUno:"una propiedad"};
+        b = a;
+        expect(a).to.be.equals(b);
+        b.propUno = "texto";
+        expect(a).to.be.equals(b);
+        b = {};
+        expect(a).to.be.deep.equals({propUno:"texto"});
+        expect(b).to.be.deep.equals({});
+      });
+    });
     describe("Arrays", function(){
-      it("Si elimino un último elemento de un array mediante 'delete' actualiza su longitud", function(){
+      it("Si elimino un elemento de un array mediante 'delete' NO actualiza su longitud", function(){
         var miArray = "miArray".split('');
         expect(miArray).to.have.length(7)
         delete miArray[4];
-        expect(miArray.length).to.have.length(6);
+        expect(miArray).to.have.length(7);
+        expect(miArray).to.have.members(['m','i','A','r','a','y']);
       });
     });
+    describe("Numbers", function(){
+      it("Se pueden representar en base-10", function(){
+        expect(32).to.be.equals(32);
+      });
+      it("Se pueden representar con notación exponencial", function(){
+        expect(1E6).to.be.equals(1000000);
+        expect(0.23E3).to.be.equals(230);
+      });
+      it("Se pueden representar con notación hexadecimal", function(){
+        expect(0xf3).to.be.equals(243);
+        expect(0xe8).to.be.equals(232);
+      });
+      it("Se pueden representar con notación octal", function(){
+        expect(0o363).to.be.equals(243);
+        expect(0o45).to.be.equals(37);
+      });
+      it("Number.isInteger indica si un número es un entero.", function(){
+        expect(Number.isInteger(32)).to.be.true;
+        expect(Number.isInteger(32.56)).to.be.false;
+      });
+      describe("Coma flotante", function(){
+        it("0.2 más 0.1 NO es 0.3", function(){
+          expect(0.2 + 0.1).to.not.be.equals(0.3);
+        });
+        it("Usando Number.epsilon 0.2 más 0.1 si es 0.3 (ES6)", function(){
+          function numbersAreEqual(a, b){
+            return Number.EPSILON 
+              ? Math.abs( a - b) < Number.EPSILON
+              : a === b;
+          }
+          expect(numbersAreEqual(0.2 + 0.1, 0.3)).to.be.true;
+        });  
+      });
+      describe("Not a number", function(){
+        function generarNaN(num){
+          return num / "un literal";
+        }
+        it("Es un número", function(){
+          expect(generarNaN(0x65)).to.be.a('number');
+          expect(generarNaN(0.68E3)).to.be.a('number');
+        });
+        it("Es distinto de NaN.", function(){
+          expect(generarNaN(25)).to.not.be.equals(NaN);
+          expect(generarNaN(89.012)).to.not.be.equals(NaN);
+        });
+        it("Es distinto de si mismo.", function(){
+          var sut = generarNaN(25);
+          expect(sut).to.not.be.equals(sut);
+        });
+        it("isNaN indica si el valor es NaN.", function(){
+          expect(isNaN(generarNaN(25))).to.be.true
+          expect(isNaN(generarNaN(89.012))).to.be.true;
+          expect(isNaN(NaN)).to.be.true;
+        });
+        it("isNaN retorna true para tipos no numéricos.", function(){
+          expect(isNaN("foo")).to.be.true;
+          expect(isNaN(function(){})).to.be.true;
+          expect(isNaN(undefined)).to.be.true;
+        });
+        it("Number.isNaN retorna false para tipos no numéricos. (ES6)", function(){
+          expect(Number.isNaN("foo")).to.be.false;
+          expect(Number.isNaN(function(){})).to.be.false;
+          expect(Number.isNaN(undefined)).to.be.false;
+        });
+      });
+      describe("Infinity", function(){
+        it("A variable overflow in a math operation generates an Infinity value", function(){
+          expect(1 / 0).to.be.equals(Number.POSITIVE_INFINITY);
+          expect(-1 / 0).to.be.equals(Number.NEGATIVE_INFINITY);
+          expect(Number.isFinite((-1 / 0))).to.be.false;
+          expect(Number.isFinite((1 / 0))).to.be.false;
+        });
+      });
+      describe("Negative Zero", function(){
+        function generateNegativeZero(){
+          return 0 * -1;
+        }
+        it("Si se pasa a string pasa como 0", function(){
+          expect(generateNegativeZero().toString()).to.be.equals("0");
+          expect(JSON.stringify(generateNegativeZero())).to.be.equals("0");
+        });
+        it("Si se pasa de string a number pasa como -0", function(){
+          expect(parseInt("-0")).to.be.equals(-0);
+          expect(+"-0").to.be.equals(-0);
+          expect(Number("-0")).to.be.equals(-0);
+          expect(JSON.parse("-0")).to.be.equals(-0);
+        });
+        it("Al compararlo con un cero positivo indica que son iguales", function(){
+          expect(-0 == 0).to.be.true;
+          expect(-0 === 0).to.be.true;
+        })
+      });
+    });
+    
   });
 })()
